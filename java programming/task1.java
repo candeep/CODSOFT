7dd7c03a48a7f1906a9c333344d4025d9619a733
R Sandeep <110334715+candeep@users.noreply.github.com>
2024-09-15 17:12:01 +0530

    Create task1.java

---
+++ java programming/task1.java
@@ -0,0 +1,57 @@
+import java.util.Scanner;
+
+public class GradeCalculator {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the number of subjects: ");
+        int numberOfSubjects = scanner.nextInt();
+        int[] marks = new int[numberOfSubjects];
+
+        for (int i = 0; i < numberOfSubjects; i++) {
+            System.out.print("Enter marks obtained in subject " + (i + 1) + " (out of 100): ");
+            marks[i] = scanner.nextInt();
+        }
+
+        int totalMarks = calculateTotalMarks(marks);
+        double averagePercentage = calculateAveragePercentage(totalMarks, numberOfSubjects);
+        char grade = calculateGrade(averagePercentage);
+        displayResults(totalMarks, averagePercentage, grade);
+        scanner.close();
+    }
+
+
+    private static int calculateTotalMarks(int[] marks) {
+        int total = 0;
+        for (int mark : marks) {
+            total += mark;
+        }
+        return total;
+    }
+
+    private static double calculateAveragePercentage(int totalMarks, int numberOfSubjects) {
+        return (double) totalMarks / numberOfSubjects;
+    }
+
+
+    private static char calculateGrade(double averagePercentage) {
+        if (averagePercentage >= 90) {
+            return 'A';
+        } else if (averagePercentage >= 80) {
+            return 'B';
+        } else if (averagePercentage >= 70) {
+            return 'C';
+        } else if (averagePercentage >= 60) {
+            return 'D';
+        } else {
+            return 'F';
+        }
+    }
+
+    private static void displayResults(int totalMarks, double averagePercentage, char grade) {
+        System.out.println("\n--- Results ---");
+        System.out.println("Total Marks: " + totalMarks);
+        System.out.printf("Average Percentage: %.2f%%\n", averagePercentage);
+        System.out.println("Grade: " + grade);
+    }
+}
